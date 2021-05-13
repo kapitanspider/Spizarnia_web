1e4adc16470ec3691faefb8b214b77a08bc65155
kapitanspider <witek.wiry@op.pl>
2021-05-13 16:49:31 +0200

    Dodawanie kategorii

---
+++ js/dodawanie_kategorii.js
@@ -0,0 +1,12 @@
+function addCategory() {
+	var category = 
+	{
+		lista: document.getElementById("lista").value,
+		nazwa: document.getElementById("nazwa").value
+	}
+	const testTableStore = Backendless.Data.of('Kategorie');
+	testTableStore.save(category)
+      .then(function (object) {
+      })
+	document.getElementById("content").innerHTML += "<h3>"+"Dodano kategorię "+category.nazwa+"</h3>";
+}
